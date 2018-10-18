4c1e449575e3097b8748f1c1ad9868fd04fd54f7
Lucas Yang <lucas.yang@vendor2-admins-MacBook-Pro-2.local>
2018-10-18 18:03:16 +0800

    add package, configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,23 @@ module.exports = {
                 use: {
                     loader: "babel-loader"
                 }
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    // "file-loader",
+                    {
+                        loader: 'file-loader?name=/img/[name].[ext]' 
+                        // options: {
+                        //     bypassOnDebug: true, // webpack@1.x
+                        //     disable: true // webpack@2.x and newer
+                        // }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
             }
         ]
     }

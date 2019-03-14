95114c2e26412b45f466f6c9aea0c82b23a1cb8b
Lucas Yang <lucas.yang@vendor2-admins-MacBook-Pro-2.local>
2019-03-14 10:57:01 +0800

    add code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,32 @@
+const path = require("path");
+
 module.exports = {
-    entry: ['babel-polyfill', "./horizon_2080/frontend/src/index.js"],
+    entry: ["babel-polyfill", "./horizon_2080/frontend/src/index.js"],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[contenthash].js"
+    },
+    optimization: {
+        runtimeChunk: "single",
+        splitChunks: {
+            chunks: "all",
+            maxInitialRequests: Infinity,
+            minSize: 0,
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name(module) {
+                        // get the name. E.g. node_modules/packageName/not/this/part.js
+                        // or node_modules/packageName
+                        const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+                        // npm package names are URL-safe, but some servers don't like @ symbols
+                        return `npm.${packageName.replace("@", "")}`;
+                    }
+                }
+            }
+        }
+    },
     module: {
         rules: [
             {
@@ -14,7 +41,7 @@ module.exports = {
                 use: [
                     // "file-loader",
                     {
-                        loader: 'file-loader?name=/img/[name].[ext]' 
+                        loader: "file-loader?name=/img/[name].[ext]"
                         // options: {
                         //     bypassOnDebug: true, // webpack@1.x
                         //     disable: true // webpack@2.x and newer
